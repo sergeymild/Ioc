541e366311ed24e5494b3676825086b16aef3664
Sergeymild <sergeymild@wisebits.com>
2019-11-17 07:29:54 +0200

    update

---
+++ core/src/main/java/com/ioc/ScopeFactory.java
@@ -0,0 +1,79 @@
+package com.ioc;
+
+/**
+ * Created by sergeygolishnikov on 28/12/2017.
+ */
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.WeakHashMap;
+
+/**
+ * Created by sergeygolishnikov on 08/11/2017.
+ */
+
+public final class ScopeFactory {
+//    private static final ScopeThread thread = new ScopeThread();
+
+    @NonNull
+    private static ScopeFactory INSTANCE = new ScopeFactory();
+    @NonNull
+    // scope root
+    //   scopeName
+    //
+    private final Map<Object, HashMap<String, HashMap<String, Object>>> scopes = new WeakHashMap<>();
+
+    public static <T> void cache(@NonNull Object key, @NonNull String scope, @NonNull String valueName, @NonNull T value) {
+//        thread.registerCleanup(key, value);
+
+        HashMap<String, HashMap<String, Object>> keyScopes = INSTANCE.scopes.get(key);
+        if (keyScopes == null) {
+            keyScopes = new HashMap<>();
+            INSTANCE.scopes.put(key, keyScopes);
+        }
+
+        HashMap<String, Object> values = keyScopes.get(scope);
+        if (values == null) {
+            values = new HashMap<>();
+            keyScopes.put(scope, values);
+        }
+
+        if (!values.containsKey(valueName)) values.put(valueName, value);
+    }
+
+    @Nullable
+    public static <T> T get(@NonNull String scope, @NonNull String valueName) {
+        for (Map.Entry<Object, HashMap<String, HashMap<String, Object>>> keyValues : INSTANCE.scopes.entrySet()) {
+            HashMap<String, Object> values = keyValues.getValue().get(scope);
+            Object o = values.get(valueName);
+            if (o != null) return (T) o;
+        }
+        return null;
+    }
+
+    @Nullable
+    public static <T> T get(@NonNull Object key, @NonNull String scope, @NonNull String valueName) {
+        HashMap<String, HashMap<String, Object>> keyScopes = INSTANCE.scopes.get(key);
+        if (keyScopes == null) return null;
+        HashMap<String, Object> values = keyScopes.get(scope);
+        if (values == null) return null;
+        Object o = values.get(valueName);
+        if (o != null) return (T) o;
+        return null;
+    }
+
+    public static void clear(boolean logException) {
+        INSTANCE.scopes.clear();
+        try {
+            Class<?> aClass = Class.forName("com.ioc.SingletonsClear");
+            Method clear = aClass.getDeclaredMethod("clearSingletons");
+            clear.invoke(null);
+        } catch (Throwable e) {
+            if (logException) e.printStackTrace();
+        }
+    }
+}
--- processor/src/test/java/com/ioc/BaseTest.kt
+++ processor/src/test/java/com/ioc/BaseTest.kt
@@ -13,11 +13,7 @@ import java.lang.ref.WeakReference
  * Created by sergeygolishnikov on 09/08/2017.
  */
 
-val keep = "android.support.annotation.Keep"
-val nonNull = "android.support.annotation.NonNull"
 val ioc = Ioc::class.java.canonicalName.toString()
-val iosProvider = IocProvider::class.java.canonicalName.toString()
-val iocLazy = IocLazy::class.java.canonicalName.toString()
 val iocLocalScope = LocalScope::class.java.canonicalName.toString()
 val named = Qualifier::class.java.canonicalName.toString()
 
@@ -49,7 +45,6 @@ val importPostInitializationAnnotation = "import ${PostInitialization::class.jav
 val importKeepAnnotation = "import androidx.annotation.Keep;"
 val importNonNullAnnotation = "import androidx.annotation.NonNull;"
 val importQualifierAnnotation = "import ${Qualifier::class.java.canonicalName};"
-val importViewModelAnnotation = "import ${ViewModel::class.java.canonicalName};"
 
 val lazyType = Lazy::class.java.canonicalName.toString()
 val providerType = Provider::class.java.canonicalName.toString()
@@ -58,6 +53,5 @@ val assetManager = AssetManager::class.java.canonicalName.toString()
 val dependency = Dependency::class.java.canonicalName.toString()
 val fragmentActivityPackage = "androidx.fragment.app"
 val lifecyclePackage = "androidx.lifecycle"
-val androidViewModel = "$lifecyclePackage.ViewModel"
 val viewModelProvider = "$lifecyclePackage.ViewModelProvider"
 val viewModelProviders = "$lifecyclePackage.ViewModelProviders"
